8a03230dc1a12df18680008dd2d9f2fb975e2ce2
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-12 14:30:00 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="DMBotNetwork",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=['aiosqlite', 'bcrypt', 'msgpack'],
     author="Angels And Demons dev team",
