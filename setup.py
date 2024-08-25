e67f7e337cf04caf30020b7c93a1b30e6cec54dc
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-25 20:43:56 +0300

    fir version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="DMBotNetwork",
-    version="0.0.81",
+    version="0.0.9",
     packages=find_packages(),
     install_requires=['aiosqlite', 'bcrypt', 'msgpack'],
     author="Angels And Demons dev team",
