6f2dbec82e4ce004011eea46338ef9ae4f3dcfd5
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-30 21:04:16 +0300

    fix import

--- DMBotNetwork/__init__.py
+++ DMBotNetwork/__init__.py
@@ -1,4 +1,5 @@
-from .client import Client
-from .server import Server
+from .client import Client  # noqa: F401
+from .server import Server  # noqa: F401
+from .server_db import ServerDB  # noqa: F401
 
 __version__ = "0.1.0"
