f2188abefb9100c37d746273b495126d816999a4
The many faced demon <154847721+themanyfaceddemon@users.noreply.github.com>
2024-09-03 19:02:31 +0300

    Update version part 1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="DMBotNetwork",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["aiosqlite", "bcrypt", "msgpack"],
     author="Angels And Demons dev team",
