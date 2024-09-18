dad752da1b06e7f5e68775e34ebf20b82002b280
themanyfaceddemon <neko-d-05@mail.ru>
2024-09-18 15:25:25 +0300

    bump version and add ServerDB into main init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="DMBotNetwork",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=["aiosqlite", "aiofiles", "bcrypt", "msgpack"],
     author="Angels And Demons dev team",
