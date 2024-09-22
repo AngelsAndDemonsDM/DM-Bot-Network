faff9d20dbf45aaf09e84be58a4f90b38fedad77
The many faced demon <154847721+themanyfaceddemon@users.noreply.github.com>
2024-09-22 16:36:29 +0300

    0.2.8 (#19)
    - Исправлена ошибка связанная с декоратором, которые не позволял
    адекватно вызывать методы под `require_access`.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="DMBotNetwork",
-    version="0.2.7",
+    version="0.2.8",
     packages=find_packages(),
     install_requires=["aiosqlite", "aiofiles", "bcrypt", "msgpack"],
     author="Angels And Demons dev team",
