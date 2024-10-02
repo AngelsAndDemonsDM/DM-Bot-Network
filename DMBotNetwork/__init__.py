5ef604579d11df6bd5f68d9aa6634568d79427c6
themanyfaceddemon <neko-d-05@mail.ru>
2024-10-02 04:10:42 +0300

    fix

--- DMBotNetwork/__init__.py
+++ DMBotNetwork/__init__.py
@@ -3,6 +3,6 @@ from .main.server import Server
 from .main.utils.cl_unit import ClUnit
 from .main.utils.decorator import require_access
 from .main.utils.server_db import ServerDB
+from .version import __version__
 
 __all__ = ["Client", "Server", "ClUnit", "require_access", "ServerDB"]
-__version__ = "0.3.1"
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import find_packages, setup
 
+from DMBotNetwork import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="DMBotNetwork",
-    version="0.3.1",
+    version=__version__,
     packages=find_packages(),
     install_requires=["aiosqlite", "aiofiles", "bcrypt", "msgpack"],
     author="Angels And Demons dev team",
     author_email="dm.bot.adm@gmail.com",
     description="Нэткод для проектов DMBot",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AngelsAndDemonsDM/DM-Bot-network",
     classifiers=[
